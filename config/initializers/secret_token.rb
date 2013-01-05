9003d14f850dd1c028fe5dab0c85fbeba1968db3
tbaley <tbaley@mac.com>
2013-01-05 16:21:35 -0500

    Wiped

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo3::Application.config.secret_token = '8372e7ddcf743ecd7a7ec23b125fd7cc1e50cf4df2fc12ca53b464b0100f4fb1cd8423e0a98fc60341bb088354c65bcfffe4625809f18e891105184a55c2568b'
